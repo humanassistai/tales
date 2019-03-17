139fedab3bf625983ebe833ae363898c484ca998
Suman  Kunwar <sumn2u@gmail.com>
2019-03-17 20:56:51 +0545

    Updates

---
+++ precache-manifest.58945c26571cc096d51d4689820f6914.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e32b167c04afd8d698a0b53b28d2c13",
+    "url": "/tales/static/media/wall-bookshelf.7e32b167.png"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/tales/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "079242267385f2c60608f2b56358f652",
+    "url": "/tales/static/media/book1.07924226.png"
+  },
+  {
+    "revision": "5f3511ad5fbb7c50dee2",
+    "url": "/tales/static/js/runtime~main.c08bbe01.js"
+  },
+  {
+    "revision": "42c6f733a91e03554af4",
+    "url": "/tales/static/js/main.738d8b75.chunk.js"
+  },
+  {
+    "revision": "4e1da6216714d4b2e451",
+    "url": "/tales/static/js/2.c8843ced.chunk.js"
+  },
+  {
+    "revision": "42c6f733a91e03554af4",
+    "url": "/tales/static/css/main.9e1266c3.chunk.css"
+  },
+  {
+    "revision": "35e83d9bea975039fd61e51ee12cc534",
+    "url": "/tales/index.html"
+  }
+];
